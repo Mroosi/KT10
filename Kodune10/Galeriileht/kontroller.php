cce9a04ecd677f1ed40e043ea71671c5552a03d7
Mroosi <madisroosi@hotmail.com>
2016-04-24 21:59:37 +0300

    Kodune10

---
+++ Kodune10/Galeriileht/kontroller.php
@@ -0,0 +1,52 @@
+﻿<?php
+    session_start();
+    if(!empty($_POST)){
+           $tulemus = implode($_POST);
+           $vastus = intval($tulemus);
+           $_SESSION["haaletas"] = $tulemus;
+        }
+    if(empty($_SESSION)){
+    $_SESSION["haaletas"] = 0;
+    }
+    require_once('head.html');
+    require_once('masiiv.php');
+        if( !empty($_GET)){
+             $vaartus = $_GET;
+        }else{
+             $vaartus = array('pealeht');
+        }
+        if(!empty($_SESSION)){
+            
+            //Kommentri allolev kood välja selleks, et sessioon kustutada.
+             /*if($_SESSION["haaletas"] == 10){
+                 session_destroy();
+                 echo "Sessioon kustutatud";
+             }*/
+             
+             if(!empty($_POST) && $_SESSION["haaletas"] <= 6){
+             require('tulemus.php');
+             }
+        }
+        $leht = implode($vaartus);
+        if(empty($_POST)){
+        switch ($leht) {
+        case 'pealeht':
+        require('pealeht.html');
+        break;
+        
+        case 'galerii':
+        require('galerii.html');
+        break;
+        
+        case 'vote':
+           require('vote.html');
+        break;
+        
+    }
+  }
+    require_once('foot.html');
+?>
+<!-- Kommentri allolev kood välja selleks, et sessioon kustutada -->
+<!--form action="kontroller.php" method="POST">
+    <input type="checkbox" name="DS" value="10">kustuta sessioon<br>
+    <input type="submit" value="kustuta"/>-->
---
+++ Kodune10/Galeriileht/tulemus.php
@@ -0,0 +1,26 @@
+﻿
+<div id="wrap">
+	<h3>Valiku tulemus</h3>
+	<p><?php if(isset($_SESSION)) {
+                require_once('masiiv.php');
+                global $galerii;
+                global $_SESSION;
+                $gPikkus = count($galerii);
+                $tulemus = implode($_SESSION);
+                $vastus = intval($tulemus);
+                $pilt = $vastus-1;
+                if($vastus > 0 && $vastus <= $gPikkus ){
+                    echo "Valisid pildi nr. ".htmlspecialchars($vastus);
+                    echo "<br>";
+                    echo "<img src=\"".$galerii[$pilt]."\"". "alt=\"nimetu".$vastus." \"". "/>";
+                }else{
+                    echo "Palun vali üks pilt!!!.";
+                }
+              }else{
+                echo "Palun vali üks pilt.";
+              }
+       ?></p>
+</div>
+<?php
+    require_once('foot.html');
+?>
